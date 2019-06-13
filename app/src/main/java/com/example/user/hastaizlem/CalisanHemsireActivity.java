5de2566529fe74eb394d5a599e1ccef90d23d137
esmabayar <esma_byr_06@hotmail.com>
2019-06-13 11:49:03 +0300

    ee

---
+++ app/src/main/java/com/example/user/hastaizlem/CalisanHemsireActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.hastaizlem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalisanHemsireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calisan_hemsire);
+    }
+}
---
+++ app/src/main/java/com/example/user/hastaizlem/KisitlamaEkleActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.hastaizlem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KisitlamaEkleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kisitlama_ekle);
+    }
+}
